70e6f0f0d5a9937480a24ef09e010d15b6bf6264
LauraCamilaValenzuela <143672214+LauraCamilaValenzuela@users.noreply.github.com>
2023-09-11 00:33:50 -0500

    Add files via upload

--- actividad_4-main/BD/baseDatos.py
+++ actividad_4-main/BD/baseDatos.py
@@ -1,10 +1,10 @@
-from pymongo.mongo_client import MongoClient
+from pymongo.mongo_client import MongoClient #Se importa mongo para hacer la conexion
 
 #CONEXIÓN
-def conexion():
-    uri = "mongodb+srv://cagomezj:1234@cluster0.cgumkrz.mongodb.net/?retryWrites=true&w=majority"
+def conexion():      #se define la funcion llamada conexion
+    uri = "mongodb+srv://cagomezj:1234@cluster0.cgumkrz.mongodb.net/?retryWrites=true&w=majority"   #se define la URI a la que se conectara
     # Create a new client and connect to the server
-    return MongoClient(uri)
+    return MongoClient(uri)  #se retorna para poder ser utilizada 
 
 #CREATE
 """ Código necesario para crear un create en MongoDB"""
@@ -12,13 +12,13 @@ def conexion():
 
 #READ
 """ Código necesario para crear un read en MongoDB"""
-def lecturaDatos():
-    client = conexion()
-    db = client.actividad4.data_real
-    data_list = []
-    for data_real_bd in db.find():
-        data_list.append(data_real_bd)
-    return data_list
+def lecturaDatos():    #se define una funcion que se llamara lectura de datos
+    client = conexion() #se crea client en la que se llama a conexion para estar en mongo
+    db = client.actividad4.data_real #se toma de la base de datos de la actividad 4 juto con la coleccion data real
+    data_list = []  #se crea una lista
+    for data_real_bd in db.find():   #en esta linea se hace un bucle que recorre toda data real
+        data_list.append(data_real_bd) #se agrega los datos obetnidos a data list
+    return data_list #se retornan los datos obtenidos de la base de datos
 
 #UPDATE
 """ Código necesario para actualizar un dato en la BD"""
--- actividad_4-main/data/data.py
+++ actividad_4-main/data/data.py
@@ -1,14 +1,14 @@
-import pandas as pd
+import pandas as pd      #se instala libreria pandas para manipular datos
 
 # Importar datos del csv
-data_teorico = pd.read_csv(r"C:\Users\Laura\Desktop\actividad_4-main\data\Data ingeniero.csv")
+data_teorico = pd.read_csv(r"C:\Users\Laura\Desktop\actividad_4-main\data\Data ingeniero.csv") #se importan datos del cvs de data ingeniero
 
 #Código general
 """ En caso que se exigiera realizar la limpieza de la data se haría aca"""
 
-def dataTeorico():
-    dataTeoricoEsfuerzo = data_teorico['Esfuerzo[kN]']
-    dataTeoricoDeformacion = data_teorico['Deformacion[mm]']
-    return dataTeoricoEsfuerzo, dataTeoricoDeformacion
+def dataTeorico():      #se define la funcion
+    dataTeoricoEsfuerzo = data_teorico['Esfuerzo[kN]']     #se extrae como dataTeoricoEsfuerzo la columna de esfuerzo
+    dataTeoricoDeformacion = data_teorico['Deformacion[mm]']  #se extrae como dataTeoricoDeformacion la columna de Deformacion
+    return dataTeoricoEsfuerzo, dataTeoricoDeformacion     #se retornan estos valores para poder ser utilizados en otra carpeta
 
 
